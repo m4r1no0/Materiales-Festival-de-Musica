0a7b6d9446f766756210fe182233f43cc5cada2f
m4r1no0 <marinoosorio97@gmail.com>
2025-02-24 17:23:16 -0500

    compilar auto

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,17 @@
-import {src, dest, watch } from 'gulp'
+import { src, dest, watch } from 'gulp'
 import * as dartSass from 'sass'
 import gulpSass from 'gulp-sass'
 
 const sass = gulpSass(dartSass)
 
-export function css( done){
+export function css( done ) {
     src('src/scss/app.scss')
-        .pipe(sass())
+        .pipe( sass().on('error', sass.logError))
         .pipe(dest('build/css'))
     
     done()
 }
 
-export function dev(){
-    watch('src/scss/app.scss',css)
+export function dev() {
+    watch('src/scss/**/*.scss', css)
 }
